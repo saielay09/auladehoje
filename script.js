193164e01217543e797d93288851ee447495673e
Estefany Ciely <101192829+saielay09@users.noreply.github.com>
2022-03-15 12:49:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
+var numSort = [];
+var cont = 0;
+var numEsco =[];
 
+function sorteio (){
+    var cont = 0;
+    numSort=[]
+    while(cont < 6){
+        let num = Math.random() * 60
+        num = Math.ceil(num);
+        if(!numSort.includes(num)){
+            numSort[cont] = num;
+            console.log(numSort)
+            cont++
+        }
+       
+    } 
+    document.getElementById("sorteados").innerHTML = numSort;
+    contAcertos();
+}
+function getValor(valor, pos){
+    valor = Number(valor);
+    if (valor <= 0 || valor > 60){
+        alert("numero inválido, digite um numero entre 1 e 60");
+        document.getElementById(`num${pos+1}`).value = "";
+    }else if(numEsco.includes(valor)){
+        alert("numero repetido, escolha um outro número")
+        document.getElementById(`num${pos+1}`).value = "";
+    }else{
+        numEsco[pos] = valor
+    console.log(numEsco)
+    } 
+}
+
+function contAcertos(){
+    let contA = 0
+    numEsco.forEach(function(value,index){
+        if (numSort.includes(value)){
+        contA  = contA + 1
+
+    }
+    
+        
+    })
+
+
+document.getElementById("acertos").innerHTML= contA
+}
